cf75818bed12fb6058e1e14d74a63a38590d35ac
Kid-Guru <birkov.alexandr@gmail.com>
2021-01-14 14:11:34 +0400

    fix prime number = 1

--- src/games/brainPrime.js
+++ src/games/brainPrime.js
@@ -3,6 +3,9 @@ import { generateNumber } from '../cli.js';
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
+  if (num === 1) {
+    return 'no';
+  }
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
       return 'no';
